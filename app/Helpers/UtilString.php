f9de7a4cf918fc8eb666010fd61ae7f29b560492
joysingh123 <joy@salesaladin.com>
2018-10-18 18:58:32 +0530

    utility function logic imporved

--- app/Helpers/UtilString.php
+++ app/Helpers/UtilString.php
@@ -40,7 +40,7 @@ class UtilString {
                 }
             }else{
                 $company_id = str_replace("https://www.linkedin.com/sales/company/", "", $url);
-                $company_id = str_replace("https://www.linkedin.com/company/", "", $url);
+                $company_id = str_replace("https://www.linkedin.com/company/", "", $company_id);
                 $company_id = str_replace("/", "",$company_id);
                 if(is_numeric($company_id) && $company_id > 0){
                     $company_id = $company_id;
