36f767b772b60231cb881ad2a3c773442c18cbea
joysingh123 <joy@salesaladin.com>
2019-02-01 11:33:39 +0530

    email varification cron added

--- app/Console/Commands/ValidateVerificationEmail.php
+++ app/Console/Commands/ValidateVerificationEmail.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use App\Helpers\UtilDebug;
 use App\Helpers\UtilConstant;
 use App\Helpers\UtilString;
+use App\EmailValidationApi;
 use App\EmailSheet;
 use App\EmailVerification;
 use App\Traits\ValidateEmailTraits;
@@ -58,19 +59,36 @@ class ValidateVerificationEmail extends Command
                 EmailSheet::where("id", $sheet_id)->update(['status' => 'Under Processing']);
                 $data_for_email_processing = EmailVerification::where('sheet_id', $sheet_id)->where('status', 'unverified')->get();
                 if ($data_for_email_processing->count() > 0) {
-                    foreach ($data_for_email_processing AS $dep) {
-                        $email = $dep->email;
-                        $v_response = $this->validateEmail($email);
-                        if($v_response['email_status'] == ""){
-                            $dep->status = 'Timeout';
-                            $dep->save();
-                        }else{
-                            $status = $v_response['email_status'];
-                            $validation_date = $v_response['validation_date'];
-                            $dep->status = $status;
-                            $dep->email_validation_date = $validation_date;
-                            $dep->save();
+                    $validation_api = EmailValidationApi::where('active','yes')->where('status','enable')->orderBy('cron_count','ASC')->get();
+                    if($validation_api->count() > 0){
+                        $validation_api = $validation_api->first();
+                        $validation_api->cron_count = $validation_api->cron_count + 1;
+                        $validation_api->save();
+                        foreach ($data_for_email_processing AS $dep) {
+                            $email = $dep->email;
+                            $v_response = $this->validateEmail($email,$validation_api);
+                            if($v_response['email_status'] == ""){
+                                $response_api_array = json_decode($v_response['response'],TRUE);
+                                if(isset($response_api_array['error']) && $response_api_array['error']['code'] == 104){
+                                    $validation_api->active = 'No';
+                                    break 2;
+                                }
+                                if(isset($response_api_array['error']) && $response_api_array['error']['code'] == 999){
+                                    $dep->status = 'Timeout';
+                                    $dep->save(); 
+                                }
+                            }else{
+                                $status = $v_response['email_status'];
+                                $validation_date = $v_response['validation_date'];
+                                $dep->status = $status;
+                                $dep->email_validation_date = $validation_date;
+                                $dep->save();
+                            }
                         }
+                        $validation_api->cron_count = $validation_api->cron_count - 1;
+                        $validation_api->save();
+                    }else{
+                        echo "No, usable api key found..";
                     }
                 }else{
                     EmailSheet::where("id", $sheet_id)->update(['status' => 'Completed']);
